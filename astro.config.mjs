5d67ecc7eb2d73fb89b5326e1fe6e2a9b49932a3
Michael Blechinger <80917019+mark88008@users.noreply.github.com>
2023-12-07 08:25:35 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://astro.m-create.at',
 	integrations: [mdx(), sitemap()],
 });
